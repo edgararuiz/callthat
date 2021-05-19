d80d3f43c5f454576b063734276157a5748941e6
Edgar Ruiz <edgararuiz@gmail.com>
2021-05-18 19:08:42 -0500

    Moves endpoint to url path, enables a regular callthat_connection work

--- R/callthat_api_get.R
+++ R/callthat_api_get.R
@@ -3,11 +3,11 @@ callthat_api_get <- function(api_connection, endpoint, query = NULL, ...) UseMet
 
 #' @export
 callthat_api_get.callthat_connection <- function(api_connection, endpoint, query = NULL, ...)  {
-  url_path <- api_connection$url
+  url_path <- paste0(api_connection$url, "/",  endpoint)
   r_safe(function() 1)
   r_safe(function(ur, enp, qry)
-    httr::GET(url = ur, path = enp, query = qry),
-    args = list(ur = url_path, enp = endpoint, qry = query)
+    httr::GET(url = ur, query = qry),
+    args = list(ur = url_path, qry = query)
   )
 }
 
