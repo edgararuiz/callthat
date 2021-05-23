7fe9781202dfec38df07c027cb6224cbdfcc85bb
Edgar Ruiz <edgararuiz@gmail.com>
2021-05-23 08:12:02 -0500

    Removes callr dep for GET

--- R/callthat_api_get.R
+++ R/callthat_api_get.R
@@ -1,24 +1,20 @@
-#' @importFrom rlang enexprs enexpr
-#' @importFrom httr add_headers
-
 #' @export
-callthat_api_get <- function(api_connection, endpoint, query = NULL, headers = list(),  eval_query = FALSE, ...) UseMethod("callthat_api_get")
+callthat_api_get <- function(api_connection, endpoint, query = NULL,
+                             headers = list(),  eval_query = FALSE, ...) {
+  UseMethod("callthat_api_get")
+}
 
 #' @export
 callthat_api_get.callthat_connection <- function(api_connection, endpoint, query = NULL,
                                                  headers = list(),  eval_query = FALSE, ...)  {
   vars <- enexprs(...)
-  if(!eval_query) {
-    qry <- enexpr(query)
-  } else {
-    qry <- query
-  }
   url_path <- paste0(api_connection$url, "/",  endpoint)
-  all_args <- c(url_path, vars, query = list(qry))
-  r_safe(function() 1)
-  r_safe(function(all_args)
-    do.call(httr::GET, all_args),
-    args = list(all_args = c(all_args, list(do.call(add_headers, headers))))
+  header_obj <- do.call(add_headers, headers)
+  GET(
+    url = url_path,
+    query = query,
+    header_obj,
+    vars
   )
 }
 
@@ -26,10 +22,14 @@ callthat_api_get.callthat_connection <- function(api_connection, endpoint, query
 callthat_api_get.callthat_rsc_connection <- function(api_connection, endpoint, query = NULL,
                                                      headers = list(),  eval_query = FALSE, ...)  {
 
+  vars <- enexpr(...)
+
   url_path <- api_connection$url
   api_key <- api_connection$key
 
-  if(!is.null(api_key)) headers <- c(headers, Authorization = paste0("Key ", api_key))
+  headers_obj <- headers
+
+  if(!is.null(api_key)) headers_obj <- c(headers_obj, Authorization = paste0("Key ", api_key))
 
   ac <- callthat_connection(url_path)
 
@@ -37,8 +37,8 @@ callthat_api_get.callthat_rsc_connection <- function(api_connection, endpoint, q
     api_connection = ac,
     endpoint = endpoint,
     query = query,
-    headers = headers,
-    eval_query = TRUE
+    headers = headers_obj,
+    vars
   )
 }
 
